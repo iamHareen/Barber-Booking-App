025bde7e177bbd66c9c3c283e21d8457f9e40006
iamMahathanthri <helanjith@jwareautomation.com>
2024-12-13 13:55:59 +0530

    login page ui completed

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -33,30 +33,113 @@ class _LogInState extends State<LogIn> {
               ),
             ),
             Container(
-              padding: const EdgeInsets.only(top: 40.0, left: 30.0, right: 30.0),
+              padding:
+                  const EdgeInsets.only(top: 40.0, left: 30.0, right: 30.0, bottom: 50.0),
               margin:
                   EdgeInsets.only(top: MediaQuery.of(context).size.height / 4),
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.only(topLeft:Radius.circular(20), topRight: Radius.circular(20)),),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20)),
+              ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text("Gmail", style: TextStyle(color: Color(0xFFB91635), fontSize: 20.0, fontWeight: FontWeight.w500),),
-                  TextField(
+                children: [
+                  const Text(
+                    "Gmail",
+                    style: TextStyle(
+                        color: Color(0xFFB91635),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const TextField(
                     decoration: InputDecoration(
                       hintText: "Gmail",
                       prefixIcon: Icon(Icons.mail_outline),
                     ),
                   ),
-                  SizedBox(height: 20,),
-                  Text("Password", style: TextStyle(color: Color(0xFFB91635), fontSize: 20.0, fontWeight: FontWeight.w500),),
-                  TextField(
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "Password",
+                    style: TextStyle(
+                        color: Color(0xFFB91635),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  const TextField(
                     decoration: InputDecoration(
                       hintText: "Password",
                       prefixIcon: Icon(Icons.password_outlined),
                     ),
                     obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        "Forgot Password",
+                        style: TextStyle(
+                            color: Color(0xFF311937),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    width: MediaQuery.of(context).size.width,
+                    decoration: BoxDecoration(
+                        gradient: const LinearGradient(colors: [
+                          Color(0xFFB91635),
+                          Color(0Xff621d3c),
+                          Color(0xFF311937),
+                        ]),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: const Center(
+                      child: Text(
+                        "SIGN IN",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(
+                            color: Color(0xFF311937),
+                            fontSize: 17,
+                            fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            color: Color(0Xff621d3c),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
                   ),
                 ],
               ),
